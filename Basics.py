014894725d25f040dd3c3eeada854c5ce69623df
DivyeshPandey667 <102403342+DivyeshPandey667@users.noreply.github.com>
2022-03-26 11:48:31 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,13 @@
+from sys import builtin_module_names
+
+
+print("Hello")
+print("My name is Divyesh")
+print("Nice to meet you")
+
+colour= "blue"
+print("My favourite colour is ", colour)
+
+name=input("What is your name ")
+print("name is", name)
+print(type(name))
