a8c240c2dfbb301063ccbd0aa9af4fb26ae4016e
DivyeshPandey667 <102403342+DivyeshPandey667@users.noreply.github.com>
2022-03-26 11:26:15 +0530

    Add files via upload

---
+++ Divyesh Calculator.py
@@ -0,0 +1,11 @@
+x=int(input("Enter your first number: "))
+y=int(input("Enter your second number: "))
+sum=x+y
+difference=x-y
+multiplication=x*y
+division=x/y
+remainder=x%y
+print("sum of two number is :", sum)
+print("difference of two number is ", difference)
+print("division of twon no. is ", division)
+print("remainder of two number is ", remainder)
