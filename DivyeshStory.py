687ae1a8acb06a9cd9d2417b553abf89e04c24fa
DivyeshPandey667 <102403342+DivyeshPandey667@users.noreply.github.com>
2022-04-03 11:57:29 +0530

    Add files via upload

---
+++ DivyeshStory.py
@@ -0,0 +1,13 @@
+Colour= "Blue Red Green Yellow"
+print(Colour[5:8])
+print(Colour[9:15])
+print(Colour[:])
+print(Colour[-6:])
+ListColour=["Blue","Red","Green","Yellow"]
+print(ListColour[2])
+print(ListColour[2][1])
+
+score = 90
+
+print('my name is %s and my score is:%d percentage'%(Colour,score))
+print('my name is',Colour +" and my score is ",str(score)+ " percentage")
