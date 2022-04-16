c5a529c3dc4cd07a6c41951384127920abe45fe9
DivyeshPandey667 <102403342+DivyeshPandey667@users.noreply.github.com>
2022-04-16 11:11:04 +0530

    Add files via upload

--- Table.py
+++ Table.py
@@ -2,3 +2,8 @@
 n=int(input("Which table do you want to print "))
 for i in range(1,11):
     print(n," X ", i," = ",n*i)
+
+    j=0
+    while(j<10):
+        j=j+1
+        print(n," X ", j, " = ",n*j)
